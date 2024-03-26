69ad75450c46cb1a4c5c52c8fc03c944bb6e5259
d4140n-4h3-1 <55861113+d4140n-4h3-1@users.noreply.github.com>
2024-03-26 13:49:25 -0500

    Update begin.sh
    Added option to not create a Swap partition.

--- begin.sh
+++ begin.sh
@@ -1,19 +1,24 @@
-# This script assumes you have already created the necessary partitions on a fresh drive. 
-
 PS3='Please select a disk type: '
 options=("SDA" "NVME" "Quit")
 select opt in "${options[@]}"
 do
 	case $opt in
 		"SDA")
-			mkfs.fat -F32 /dev/sda1
+  			mkfs.fat -F32 /dev/sda1
 			mkfs.ext4 /dev/sda3
 			mkswap /dev/sda2
-			mount /dev/sda3 /mnt
+  			mount /dev/sda3 /mnt
 			ls /mnt
 			swapon /dev/sda2
 		;;
-
+  
+		"SDA No Swap")
+			mkfs.fat -F32 /dev/sda1
+			mkfs.ext4 /dev/sda2
+			mount /dev/sda2 /mnt
+			ls /mnt
+		;;
+  
 		"NVME")
 			mkfs.fat -F32 /dev/nvme0n1p1
 			mkfs.ext4 /dev/nvme0n1p3
@@ -23,9 +28,16 @@ do
 			swapon /dev/nvme0n1p2
 		;;
 
+		"NVME No Swap")
+			mkfs.fat -F32 /dev/nvme0n1p1
+			mkfs.ext4 /dev/nvme0n1p2
+			mount /dev/nvme0n1p2
+			ls /mnt
+		;;
+
 		"Quit")
 			break
 		;;
 		*) echo "Invalid Option $REPLY";;
 	esac
-done
+ done
